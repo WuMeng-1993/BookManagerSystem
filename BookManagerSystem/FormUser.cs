cc0becb0d9e78aaccc57acef2752281257580aca
wu-meng <wumeng1993@outlook.com>
2024-12-17 23:11:50 +0800

    创建FormUser页面

---
+++ BookManagerSystem/FormUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookManagerSystem
+{
+    public partial class FormUser : Form
+    {
+        public FormUser()
+        {
+            InitializeComponent();
+        }
+
+        private void FormUser_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
